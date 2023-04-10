e65cab3e3de08bc6e4bfb18d01166576ea3ca239
Karen Kitchens <karen.kitchens@unity3d.com>
2023-04-10 10:13:22 -0500

    add port for api to listen

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const PORT = 4000
+
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `)
+})
+
 app.post("/comments", async (req, res) => {
     const id = uuid();
     const content = req.body.content;
